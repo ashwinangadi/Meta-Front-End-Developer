f4fc9873c1982c3809399b437a880da301546fc4
ashwinangadi <ashwin.angadi1@gmail.com>
2023-01-25 12:01:18 +0530

    Create Creating arrays and objects.js

---
+++ Programming with JavaScript/Creating arrays and objects.js
@@ -0,0 +1,15 @@
+var clothes = [];
+clothes.push("banana");
+clothes.push("apple");
+clothes.push("orange");
+clothes.push("chichoo");
+clothes.push("berries");
+console.log(clothes)
+clothes.pop();
+console.log(clothes)
+clothes.push("pants");
+console.log(clothes[2])
+var favCar = {};
+favCar.color = "Red";
+favCar.convertable = true;
+console.log(favCar)
